f376043e3eac6077a7033fde52431fc61265d68a
hguldal <hguldal@gmail.com>
2016-01-27 09:55:31 +0200

    ae4ecf

---
+++ Shared/Javascript/Dil/en/Odev/Liste.js
@@ -0,0 +1,28 @@
+var mesajlar = {
+    islemtamam:'Ok',
+    dosyaboyutubuyuk:'File size is too big',
+    yetersizdepolamaalani:'Your storage space is not enough. Please upgrade you account type',
+    derssecilmedi:'Select course',
+    basliksecilmedi:'Enter the assigment title',
+    gozat:'Explore',
+    odevayarlari:'Assigment Settings',
+    odevinigonderenler:'Assigment Senders',
+    odevbasariylaeklendi:'Assigment was added successfully',
+    verilmesirasindabirhataolustu:'An error occured when assigment adding',
+    odevsilinecek:'Assigment will be delete. Are you sure ?',
+    islemsirasindabirhataolustu:'Unexpected error'
+    
+   
+};
+
+var ayarlar = {
+    datatablelangurl:'',
+    dtformat:'MM-DD-YYYY',
+    dtpickerlocale: {
+            applyLabel: 'Ok',
+            cancelLabel: 'Cancel',
+            daysOfWeek: ['Sn', 'Sun', 'Mon', 'Tue', 'Thu', 'Fri', 'Sat'],
+            monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            firstDay: 1
+        }
+};;
