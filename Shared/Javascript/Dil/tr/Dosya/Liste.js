f2acad8fdd0427c2bf319bb8f6b0d8b9f241854b
hguldal <hguldal@gmail.com>
2016-02-02 16:20:17 +0200

    23aedcf

---
+++ Shared/Javascript/Dil/tr/Dosya/Liste.js
@@ -0,0 +1,15 @@
+var mesajlar = {
+    
+     dosyaboyutubuyuk:'Seçtiğin dosyanın boyutu çok büyük',
+     dosyaicinyetersizalan:'Bu dosyayı yükleyebilmek için yeterli depolama alanı yok. Depolama alanınızı arttırmak için lütfen hesap türünüzü yükseltin.',
+     dosyasecilmedi:'Dosya seçmediniz.',
+     dosyabasariylaeklendi:'Dosyayı başarıyla ekledin',
+     dosyaeklenirkenhata:'Dosya eklenirken bir hata oluştu.',
+     dosyasilinecek:'Dosyanız silinecek. Emin misiniz ?',
+     islemtamam:'İşlem Tamam',
+     islemsirasindahata:'İşlem sırasında bir hata meydana geldi'
+};
+
+var ayarlar = {
+    datatablelangurl: '//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Turkish.json'
+};
