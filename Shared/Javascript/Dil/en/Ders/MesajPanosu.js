ec97cc296bed3a1caa15b9ca159e4cdc5bb009e4
hguldal <hguldal@gmail.com>
2016-02-25 15:13:51 +0200

    1aedcf

--- Shared/Javascript/Dil/en/Ders/MesajPanosu.js
+++
@@ -1,8 +0,0 @@
-var mesajlar = {
-    islemtamam:'OK',
-    azonce:'Just now',
-    mesajpaylasilirkenhata:'An error occurred when sending the message',
-    mesajsilinecek:'Message will be deleted. Are you sure ?',
-    islemsirasindahata:'Unexpected error !'
-
-};
--- Shared/Javascript/Dil/tr/Ders/MesajPanosu.js
+++
@@ -1,8 +0,0 @@
-var mesajlar = {
-    islemtamam:'İşlem Tamam',
-    azonce:'Az Önce',
-    mesajpaylasilirkenhata:'Mesaj panoda paylaşılırken bir hata meydana geldi',
-    mesajsilinecek:'Mesaj silinecek. Emin misin ?',
-    islemsirasindahata:'İşlem sırasında bir hata meydana geldi'
-
-};
