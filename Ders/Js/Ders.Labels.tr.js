c87c5055c4f1214196103d6c0b1d7719981155dd
hguldal <hguldal@gmail.com>
2016-02-25 13:26:10 +0200

    1aedcf

--- Ders/Js/Ders.Labels.tr.js
+++ Ders/Js/Ders.Labels.tr.js
@@ -5,69 +5,20 @@ var mesajlar = {
     dersayarlarikaydedilirkenhata:'Ders ayarları kaydedilirken bir hata meydana geldi !',
     silinecekkodusec:'Silinecek kodu listeden seçmelisin',
     beklenmeyenhata:'Beklenmeyen bir hata meydana geldi',
-    durdur:'Durdur'
-};
-
-var ayarlar = {
-    datatablelangurl:'//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Turkish.json'
-};
-
-var mesajlar = {
-    islemtamam:'İşlem Tamam',
-    islemsirasindahata: 'İşlem esnasında bir hata meydana geldi',
+    durdur:'Durdur',
     ogrencibasariylaonaylandi:'Öğrenci başarıyla onaylandı',
     ogrencierisimdurdurulacak:'Öğrencinin derse olan erişimi durdurulacak. Emin misin ?',
     ogrenciderseerisimdurduruldu:'Öğrencinin derse olan erişimini durdurdun',
     derstakipediliyorveyaistekgonderilmis:'Ders takip ediliyor veya takip için daha önce bir istek gönderilmiş',
-    takipistegigonderildi:'Dersi takip etmek için sorumlusuna takip isteği başarıyla gönderildi'
-};
-
-var ayarlar = {
-    datatablelangurl:'//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Turkish.json'
-};
-
-var mesajlar = {
-    islemtamam:'İşlem Tamam',
+    takipistegigonderildi:'Dersi takip etmek için sorumlusuna takip isteği başarıyla gönderildi',
     erisimiznikaldirilacak:'Seçtiğin dosyaya olan erişim izni, bu dersi alan öğrenciler için kaldırılacak (Not: Bu işlem dosyayı silmez). Emin misin?',
-    islemsirasindahata:'İşlem sırasında bir hata meydana geldi',
+
     dosyaboyutubuyuk:'Seçtiğin dosyanın boyutu çok büyük',
     dosyaicinyetersizalan:'Bu dosyayı yükleyebilmek için yeterli depolama alanı yok. Depolama alanınızı arttırmak için lütfen hesap türünüzü yükseltin.',
     dosyasecilmedi:'Dosya seçmediniz.',
     dosyabasariylaeklendi:'Dosyayı başarıyla ekledin',
     dosyaeklenirkenhata:'Dosya eklenirken bir hata oluştu.',
-    dosyazatenekli:'Bu dosyayı derse zaten eklenmişsin'
-};
-
-var ayarlar = {
-    datatablelangurl:'//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Turkish.json'
-}
-
-var mesajlar = {
-    
-    islemsirasindahata:'İşlem sırasında bir hata meydana geldi',
-    dersadigirilmedi:'Dersin adını girmediniz',
-    gozat:'Gözat',
-    ayarlar:'Ayarlar',
-    dersiolusturuldu:' dersi oluşturuldu',
-    tamam:'Tamam',
-    dersadibos:'Hata: Ders adı boş bırakılamaz',
-    derstakibirakilacak:'Dersi takip etmeyi bırakmak üzeresin. Emin misin ? ',
-    erisimkodugirilmedi:'Erişim kodunu girmedin',
-    dersekatilimbasarisizoldu:'Derse katılımın başarısız oldu. Lütfen erişim kodunu kontrol et.'
-}
-
-var mesajlar = {
-    islemtamam:'İşlem Tamam',
-    azonce:'Az Önce',
-    mesajpaylasilirkenhata:'Mesaj panoda paylaşılırken bir hata meydana geldi',
-    mesajsilinecek:'Mesaj silinecek. Emin misin ?',
-    islemsirasindahata:'İşlem sırasında bir hata meydana geldi'
-
-};
-
-
-var mesajlar = {
-    islemtamam:'İşlem Tamam',
+    dosyazatenekli:'Bu dosyayı derse zaten eklenmişsin',
     basliksecilmedi:'Ödev için bir başlık seçmelisin.',
     gozat:'Gözat',
     odevayarlari:'Ödev Ayarları',
@@ -75,9 +26,21 @@ var mesajlar = {
     odevbasariylaolusturuldu:'Ödev başarıyla oluşturuldu',
     odeveklenirkenhata:'Ödev eklenirken bir hata oluştu.',
     odevsilinecek:'Ödev silinecek. Emin misin ?',
-    islemsirasindahata:'İşlem sırasında bir hata meydana geldi'
-
     
+    dersadigirilmedi:'Dersin adını girmediniz',
+    
+    ayarlar:'Ayarlar',
+    dersiolusturuldu:' dersi oluşturuldu',
+    tamam:'Tamam',
+    dersadibos:'Hata: Ders adı boş bırakılamaz',
+    derstakibirakilacak:'Dersi takip etmeyi bırakmak üzeresin. Emin misin ? ',
+    erisimkodugirilmedi:'Erişim kodunu girmedin',
+    dersekatilimbasarisizoldu:'Derse katılımın başarısız oldu. Lütfen erişim kodunu kontrol et.',
+    azonce:'Az Önce',
+    mesajpaylasilirkenhata:'Mesaj panoda paylaşılırken bir hata meydana geldi',
+    mesajsilinecek:'Mesaj silinecek. Emin misin ?'
+   
+
 };
 
 var ayarlar = {
