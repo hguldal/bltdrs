f03774ff09781ecc6dad5008def36c9e79187ecf
hguldal <hguldal@gmail.com>
2016-02-27 00:14:55 +0200

    2aedcf

--- Shared/Javascript/Dil/en/Aktiviteler.js
+++
@@ -1,9 +0,0 @@
-var mesajlar = {
-   silinmeesnasindahata:'An error occured while deleting',
-   islemsirasindahata:'Unexpected error !',
-   aktivitesilinecek:'Activity will be delete. Are you sure ?',
-   begenmektenvazgec:'Cancel Like',
-   begen:'Like',
-   begenisayisi:'Likes',
-   odevgonderimikapatilacak:'Are you sure ?'
-};
--- Shared/Javascript/Dil/tr/Aktiviteler.js
+++
@@ -1,9 +0,0 @@
-var mesajlar = {
-   silinmeesnasindahata:'Silinme esnasında bir hata meydana geldi',
-   islemsirasindahata:'İşlem sırasında bir hata meydana geldi',
-   aktivitesilinecek:'Aktivite silinecek emin misin ?',
-   begenmektenvazgec:'Beğenmekten vazgeç',
-   begen:'Beğen',
-   begenisayisi:'Beğeni sayısı',
-   odevgonderimikapatilacak:'Emin misin ?'
-};
