dfb093ae8adc03e0aa7987b288466e8b6569dc74
hguldal <hguldal@gmail.com>
2016-02-26 23:57:41 +0200

    123aedc

---
+++ Panel/Js/Panel.Labels.en.js
@@ -0,0 +1,9 @@
+var mesajlar = {
+   silinmeesnasindahata:'An error occured while deleting',
+   islemsirasindahata:'Unexpected error !',
+   aktivitesilinecek:'Activity will be delete. Are you sure ?',
+   begenmektenvazgec:'Cancel Like',
+   begen:'Like',
+   begenisayisi:'Likes',
+   odevgonderimikapatilacak:'Are you sure ?'
+};
