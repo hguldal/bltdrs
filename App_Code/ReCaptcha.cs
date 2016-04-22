e35cb978d6099c24eb12b8155871d57b81257af1
hguldal <hguldal@gmail.com>
2016-04-22 14:27:21 +0300

    23aecfd

---
+++ App_Code/ReCaptcha.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using Newtonsoft.Json;
+
+    public class ReCaptchaClass
+    {
+        public static string Validate(string EncodedResponse)
+        {
+            var client = new System.Net.WebClient();
+
+            string PrivateKey = "6LdM5hwTAAAAAAAjS6NQWI7uVXlCddNNhSJ__Slu";
+
+            var GoogleReply = client.DownloadString(string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", PrivateKey, EncodedResponse));
+
+            var captchaResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<ReCaptchaClass>(GoogleReply);
+
+            return captchaResponse.Success;
+        }
+
+        [JsonProperty("success")]
+        public string Success
+        {
+            get { return m_Success; }
+            set { m_Success = value; }
+        }
+
+        private string m_Success;
+        [JsonProperty("error-codes")]
+        public List<string> ErrorCodes
+        {
+            get { return m_ErrorCodes; }
+            set { m_ErrorCodes = value; }
+        }
+
+
+        private List<string> m_ErrorCodes;
+    }
